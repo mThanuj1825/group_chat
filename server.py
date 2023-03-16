11045f48d42f8e5975c6b0ab5434ecd458b6a8a6
unknown <thanujmullaguri1825@gmail.com>
2023-03-16 19:11:21 +0530

    mThanuj1825

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import threading
 HOST = "127.0.0.1"
 PORT = 55555
 ADDRESS = (HOST, PORT)
+FORMAT = "utf-8"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDRESS)
@@ -12,22 +13,42 @@ server.listen()
 connected_clients = {}
 
 
-def broadcast_message(message):
+def broadcast_message(sender_client, message):
     global connected_clients
     for client in connected_clients:
-        client.send(message)
+        if client != sender_client:
+            client.send(message)
 
 
 def handle_client(client):
-    pass
+    global connected_clients
+    username = client.recv(1024).decode(FORMAT)
+    connected_clients[client] = username
+    print(f"[NEW CONNECTION] {username} connected.")
+
+    while True:
+        try:
+            message = client.recv(1024).decode(FORMAT)
+            if message:
+                print(f"[{username}] {message}")
+                broadcast_message(
+                    client, f"[{username}] {message}".encode(FORMAT))
+        except:
+            print(f"[DISCONNECTED] {username} disconnected.")
+            del connected_clients[client]
+            broadcast_message(client,
+                              f"[SERVER] {username} has left the chat.".encode(FORMAT))
+            break
 
 
 def main():
     global connected_clients
-    client, client_address = server.accept()
-    connected_clients[client] = ""
+    while True:
+        client, client_address = server.accept()
+        connected_clients[client] = ""
 
-    client_thread = threading.Thread(target=handle_client, args=(client, ))
-    client_thread.start()
+        client_thread = threading.Thread(target=handle_client, args=(client, ))
+        client_thread.start()
 
-    print(f"[TOTAL CONNECTIONS] Online users: {threading.active_count() - 1}")
+        print(
+            f"[TOTAL CONNECTIONS] Online users: {threading.active_count() - 1}")
