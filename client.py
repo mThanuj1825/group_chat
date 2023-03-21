4da6f3fa77c26b0d9f85f56fac8f587d017faa5e
unknown <thanujmullaguri1825@gmail.com>
2023-03-21 19:47:05 +0530

    mThanuj1825

--- client.py
+++ client.py
@@ -10,69 +10,87 @@ FORMAT = "utf-8"  # format in which encoding and decoding should take place
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_connected = False
 
-root = tkinter.Tk()
 
-root.title("Group Chat App")
-
-output_area = tkinter.Text(root, state="disabled")
-input_area = tkinter.Entry(root)
-
-def send_message():
-    global client
-    message = input_area.get()
-    input_area.delete(0, 'end')
-    client.send(message.encode(FORMAT))
+def logo_screen():
+    root = tkinter.Tk()
+    logo = tkinter.PhotoImage(file="logo.png")
+    root.geometry("653x465")
+    root.resizable(False, False)
+    label = tkinter.Label(root, image=logo)
+    label.pack()
+    root.after(2000, root.destroy)
+    root.mainloop()
 
 
-def connect_to_server():
-    global ADDRESS, client, server_connected, input_area, connect_button
-    host_ip = input_area.get()
-    input_area.delete(0, 'end')
-    ADDRESS = (host_ip, PORT)
-    client.connect(ADDRESS)
-    connect_button.destroy()
-    server_connected = True
-    input_area.bind("<Return>", lambda send_event: send_message())
-    
+def main_window():
+    global host, PORT, ADDRESS, FORMAT, client, server_connected
+    root = tkinter.Tk()
+    root.geometry("653x465")
+    root.resizable(False, False)
+    root.title("Group Chat App")
 
+    output_area = tkinter.Text(root, state="disabled")
+    input_area = tkinter.Entry(root)
 
-def recieve_messages():
-    global client, server_connected
-    while True:
+    def send_message():
+        global host, PORT, ADDRESS, FORMAT, client, server_connected
+        message = input_area.get()
+        input_area.delete(0, 'end')
+        client.send(message.encode(FORMAT))
+
+    def connect_to_server():
+        global host, PORT, ADDRESS, FORMAT, client, server_connected
+        host_ip = input_area.get()
+        input_area.delete(0, 'end')
+        ADDRESS = (host_ip, PORT)
         try:
-            if client and server_connected:
-                msg = client.recv(1024).decode(FORMAT)
-                if not msg:
-                    break
-                output_area.config(state="normal")
-                output_area.insert(tkinter.END, msg)
-                output_area.config(state="disabled")
-        except ConnectionAbortedError:
-            break
+            client.connect(ADDRESS)
+            connect_button.destroy()
+            server_connected = True
+            input_area.bind("<Return>", lambda send_event: send_message())
+        except:
+            output_area.config(state="normal")
+            output_area.insert(tkinter.END, "Invalid IP Address\n")
+            output_area.config(state="disabled")
+
+    def recieve_messages():
+        global host, PORT, ADDRESS, FORMAT, client, server_connected
+        while True:
+            try:
+                if client and server_connected:
+                    msg = client.recv(1024).decode(FORMAT)
+                    if not msg:
+                        break
+                    output_area.config(state="normal")
+                    output_area.insert(tkinter.END, msg)
+                    output_area.config(state="disabled")
+            except ConnectionAbortedError:
+                break
+
+    def on_closing():
+        global host, PORT, ADDRESS, FORMAT, client, server_connected
+        if client:
+            client.close()
+        server_connected = False
+        root.destroy()
+        exit(0)
+
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+
+    send_button = tkinter.Button(root, text='Send', command=send_message)
+    connect_button = tkinter.Button(
+        root, text='Connect', command=connect_to_server)
+
+    output_area.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
+    input_area.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky="EW")
+    send_button.grid(row=2, column=0, padx=5, pady=5)
+    connect_button.grid(row=2, column=0, padx=5, pady=5)
+
+    recieve_thread = threading.Thread(target=recieve_messages)
+    recieve_thread.start()
+
+    root.mainloop()
 
 
-def on_closing():
-    global client, server_connected
-    if client:
-        client.close()
-    server_connected = False
-    root.destroy()
-    exit(0)
-
-
-send_button = tkinter.Button(root, text='Send', command=send_message)
-connect_button = tkinter.Button(
-    root, text='Connect', command=connect_to_server)
-
-output_area.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
-input_area.grid(row=1, column=0, columnspan = 2,padx=5, pady=5, sticky="EW")
-send_button.grid(row=2, column=0, padx=5, pady=5)
-connect_button.grid(row=2, column=0, padx=5, pady=5)
-
-
-recieve_thread = threading.Thread(target=recieve_messages)
-recieve_thread.start()
-
-
-root.protocol("WM_DELETE_WINDOW", on_closing)
-root.mainloop()
+logo_screen()
+main_window()
